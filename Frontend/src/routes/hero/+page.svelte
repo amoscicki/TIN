cb1bcc11a60ab2fe41a0ca450472c390d2e03961
Arkadiusz Mościcki <s24095@pjwstk.edu.pl>
2023-11-02 22:56:07 +0000

    .gitignore update

---
+++ Frontend/src/routes/hero/+page.svelte
@@ -0,0 +1,15 @@
+<div class="container h-full  mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Hero</h3>
+        <p class="card-description">
+            A simple hero component with a background image.
+        </div>
+</div>
+    </div>
+</div>
+
+<style>
+    
+</style>
