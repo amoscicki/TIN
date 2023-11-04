e6a425cb979a8bf6bc148e9ff9e923b275789259
Arkadiusz Mościcki <s24095@pjwstk.edu.pl>
2023-11-04 20:14:00 +0000

    configured palette, added initial routes

---
+++ Frontend/src/routes/dashboard/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { AppRail, Avatar } from "@skeletonlabs/skeleton";
+</script>
+
+<AppRail>
+  <svelte:fragment slot="lead">
+    <Avatar
+      src="https://source.unsplash.com/random?face"
+      width="w-16"
+    />
+  </svelte:fragment>
+</AppRail>
