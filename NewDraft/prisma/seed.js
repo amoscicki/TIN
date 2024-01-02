0371318f227b04c6c434d2c4848fd81228da67c2
amoscicki <arkadiuszmoscicki@gmail.com>
2024-01-02 14:17:42 +0000

    created seed.js

---
+++ NewDraft/prisma/seed.js
@@ -0,0 +1,41 @@
+import prisma from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+const db = new prisma.PrismaClient();
+
+const seedData = async () => {
+  await db.role.create({
+    data: {
+      name: 'admin'
+    }
+  });
+
+  await db.role.create({
+    data: {
+      name: 'user'
+    }
+  });
+
+  await db.user.create({
+    data: {
+      email: 'admin@fq.admin',
+      passwordHash: await bcrypt.hash('admin', 10),
+      userAuthToken: crypto.randomUUID(),
+      role: { connect: { name: 'admin' } }
+    }
+  });
+
+  await db.user.create({
+    data: {
+      email: 'user@fq.user',
+      passwordHash: await bcrypt.hash('user', 10),
+      userAuthToken: crypto.randomUUID(),
+      role: { connect: { name: 'user' } }
+    }
+  });
+};
+
+seedData().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
